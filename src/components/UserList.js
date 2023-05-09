24939a69cdc66b3ba7227f39cd761ef072e73e78
Gal <galhayne@gmail.com>
2023-05-09 18:06:43 +0300

    show users on screen add delay and error msg

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,9 +1,40 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsrs } from '../store';
+import Skeleton from './Skeleton';
+
 function UserList() {
-    return (
-        <div>
-            UserList
+
+    const dispatch = useDispatch();
+
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    })
+
+    useEffect(() => {
+        dispatch(fetchUsrs());
+    }, [dispatch])
+
+    if (isLoading) {
+        return <Skeleton times={6} className="h-10 w-full" />
+    }
+
+    if (error) {
+        return <div>
+            Error fetching data...
         </div>
-    )
+    }
+
+    const renderUsers = data.map((user) => {
+        return <div key={user.id} className='mb-2 border rounded'>
+            <div className='flex p-2 justify-between items-center cursor-pointer'>
+                {user.name}
+            </div>
+        </div>
+    });
+
+    return <div>{renderUsers}</div>
+
 }
 
 export default UserList;
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsrs } from "../thunks/fetchUsers";
 
 const userSlice = createSlice({
     name: 'users',
@@ -7,7 +8,20 @@ const userSlice = createSlice({
         isLoading: false,
         error: null
     },
-    reducers: {}
+    extraReducers(builder) {
+        builder.addCase(fetchUsrs.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsrs.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsrs.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+
+    }
 });
 
 export const userReducer = userSlice.reducer;
